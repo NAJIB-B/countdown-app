35eb9f436bcb38930abc8e422994debce67bfa3b
NAJIB-B <najibajibasu@gmail.com>
2023-12-12 23:56:38 +0100

    cleaning up dead code

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// let new_year = new Date ("2023-12-25T00:00:00.000Z");
-// let today = new Date();
-// let milliseconds = new_year.getTime() - today.getTime();
-// let seconds = Math.floor((milliseconds / 1000) % 60) + "";
-// let minutes = Math.floor( (milliseconds / 1000 / 60) % 60) + "";
-// let hours = Math.floor((milliseconds / 1000/ 60 / 60) % 24) + "";
-// let days =   Math.floor(milliseconds / 1000/ 60 / 60 / 24) + "";
-// console.log("New years date is ",new_year.toString());
-// console.log("today's date and time is ", today.toString());
-// console.log("the difference is ", milliseconds);
-// console.log(`the time left is ${days.padStart(2, "0")}:${hours.padStart(2, "0")}:${minutes.padStart(2, "0")}:${seconds.padStart(2, "0")}`);
 
 const day_el = document.getElementById("day");
 const hour_el = document.getElementById("hour");
